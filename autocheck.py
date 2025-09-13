d11a9139c11d973bbf6b667b77dda0969c008bbf
Serg Kalina <kalinasergey911@gmail.com>
2025-09-13 14:58:29 +0300

    Автоперевірка 3

--- autocheck.py
+++ autocheck.py
@@ -236,3 +236,127 @@ write_contacts_to_file('contacts.json', contacts)
 print(read_contacts_from_file('contacts.json'))
 # Виведення
 # {'name': 'Allen Raymond', 'email': 'nulla.ante@vestibul.co.uk', 'phone': '(992) 914-3792', 'favorite': False}
+
+
+"""                                  Автоперевірка 3
+
+                        Теорія автоперевірки 3
+                        
+Another very commonly used information exchange format is the tabular representation. The csv format is an open format 
+for storing tabular data that any editor supports. The csv format is essentially the same text file, but with the condition 
+that all information in it is divided into columns and rows by delimiter characters. Typically, a comma separates columns 
+and rows by a newline character. But you can use any other combination of characters.
+
+Python supports working with tabular data in the csv format. The csv package is provided as standard."""
+
+import csv
+
+with open('eggs.csv', 'w', newline='') as fh:
+    spam_writer = csv.writer(fh)
+    spam_writer.writerow(['Spam'] * 5 + ['Baked Beans'])
+    spam_writer.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+
+with open('eggs.csv', newline='') as fh:
+    spam_reader = csv.reader(fh)
+    for row in spam_reader:
+        print(', '.join(row))
+
+"""
+As a result of executing this code, the eggs.csv file appeared in the working folder. If you open it with a table editor, it
+will open as a table.
+
+There are two auxiliary classes in the csv package for working with tabular data that make it a little more convenient:"""
+
+import csv
+
+with open('names.csv', 'w', newline='') as fh:
+    field_names = ['first_name', 'last_name']
+    writer = csv.DictWriter(fh, fieldnames=field_names)
+    writer.writeheader()
+    writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+    writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+    writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+
+with open('names.csv', newline='') as fh:
+    reader = csv.DictReader(fh)
+    print(reader.fieldnames)
+    for row in reader:
+        print(row['first_name'], row['last_name'])
+
+""" The DictWriter and DictReader classes allow you to work with table rows as dictionaries, where column names (first row) 
+are used as keys.
+
+Thus, you can use Python to generate tabular data and import data from tables.
+
+                        
+                        Завдання до автоперевірки 3
+                        
+There is a list, each element of which is a dictionary with user contacts of the following type:
+
+    {
+    "name": "Allen Raymond",
+    "email": "nulla.ante@vestibul.co.uk",
+    "phone": "(992) 914-3792",
+    "favorite": False,
+}
+The dictionary contains the name of the user name, their email, phone number, and the favorite property (indicating whether the 
+contact is a favorite).
+
+Develop two functions to serialize and deserialize a list of contacts using a csv package and store the resulting data in a 
+text file.
+
+The first function write_contacts_to_file takes two parameters: filename — the name of the file, and contacts — the list of 
+contacts. This function saves the specified list in a csv file.
+
+The structure of the csv file should be as follows:
+
+name,email,phone,favorite
+Allen Raymond,nulla.ante@vestibul.co.uk,(992) 914-3792,False
+Chaim Lewis,dui.in@egetlacus.ca,(294) 840-6685,False
+Kennedy Lane,mattis.Cras@nonenimMauris.net,(542) 451-7038,True
+Wylie Pope,est@utquamvel.net,(692) 802-2949,False
+Cyrus Jackson,nibh@semsempererat.com,(501) 472-5218,True
+Please note that the first line of the file contains the headers, which are the names of the keys.
+
+The second function, read_contacts_from_file, reads, converts, and returns the specified list of contacts from the file filename 
+saved earlier by the `write_contacts_to_file function.
+
+Notice. When reading a csv file, we get the favorite dictionary property as a string, i.e., for example, favorite='False'. You need 
+to convert it back to a logical expression to make it favorite=False."""
+
+import csv
+
+
+def write_contacts_to_file(filename, contacts):
+    with open(filename, 'w', newline='') as file:
+        fieldnames = ['name', 'email', 'phone', 'favorite']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        for contact in contacts:
+            writer.writerow(contact)
+    
+
+def read_contacts_from_file(filename):
+    with open(filename, 'r', newline='') as file:
+        reader = csv.DictReader(file)
+        contacts = []
+        for row in reader:
+            row['favorite'] = True if row['favorite'] == 'True' else False
+            contacts.append(row)
+    return contacts
+contacts =[ 
+            {"name": "Allen Raymond","email": "nulla.ante@vestibul.co.uk", "phone": "(992) 914-3792", "favorite": False},
+            {"name": "Chaim Lewis","email": "dui.in@egetlacus.ca", "phone": "(294) 840-6685", "favorite": False},
+            {"name": "Kennedy Lane","email": "mattis.Cras@nonenimMauris.net", "phone": "(542) 451-7038", "favorite": True},
+            {"name": "Wylie Pope","email": "est@utquamvel.net", "phone": "(692) 802-2949", "favorite": False},
+            {"name": "Cyrus Jackson","email": "nibh@semsempererat.com", "phone": "(501) 472-5218", "favorite": True},
+]
+write_contacts_to_file('contacts.csv', contacts)
+print(read_contacts_from_file('contacts.csv'))
+# Виведення
+# [{'name': 'Allen Raymond', 'email': 'nulla.ante@vestibul.co.uk', 'phone': '(992) 914-3792', 'favorite': False}, 
+# {'name': 'Chaim Lewis', 'email': 'dui.in@egetlacus.ca', 'phone': '(294) 840-6685', 'favorite': False}, 
+# {'name': 'Kennedy Lane', 'email': 'mattis.Cras@nonenimMauris.net', 'phone': '(542) 451-7038', 'favorite': True}, 
+# {'name': 'Wylie Pope', 'email': 'est@utquamvel.net', 'phone': '(692) 802-2949', 'favorite': False}, 
+# {'name': 'Cyrus Jackson', 'email': 'nibh@semsempererat.com', 'phone': '(501) 472-5218', 'favorite': True}]
+
