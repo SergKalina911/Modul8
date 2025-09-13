156ef7f788c43699fc94451dd2a92e7bdaac9702
Serg Kalina <kalinasergey911@gmail.com>
2025-09-13 13:45:30 +0300

    Автоперевірка 2

--- autocheck.py
+++ autocheck.py
@@ -102,3 +102,137 @@ print(read_contacts_from_file('contacts.bin'))
 # {'name': 'Allen Raymond', 'email': 'nulla.ante@vestibul.co.uk', 'phone': '(992) 914-3792', 'favorite': False}
 
 
+"""                                     Автоперевірка 2
+
+                        Теорія автоперевірки 2
+                        
+The JSON protocol (which stands for JavaScript Object Notation) is a prevalent transmission protocol on the Internet. 
+This protocol has several advantages:
+
+- simple, easy to implement;
+- readable;
+- relatively compact (there are much more economical protocols).
+
+The first advantage made JSON universal, any modern programming language supports JSON. And if not, you can easily 
+implement JSON support yourself.
+
+JSON also has some disadvantages:
+
+- imited set of types;
+- resource-intensive (there are more resource-intensive protocols).
+
+JSON supports the following data types:
+
+- a record (like a dictionary in Python), only strings can be used as a key, the value can be any JSON type;
+- array (like a list in Python);
+- number (there is no difference between integers or fractions);
+- literal (True, False, None);
+- string.
+
+As in Python, a record and an array can contain nested records and/or dictionaries of any nesting depth.
+
+You should be careful with type conversions when working with JSON in Python. Tuples become lists when unpacked from JSON. 
+Dictionary keys, if they were numbers, become strings.
+
+Python supports JSON and comes with the json package as standard, which contains everything you need to work with JSON.
+
+The dump and load methods save data to a writeable file and read from a readable file.
+
+import json
+
+some_data = {'key': 'value', 2: [1, 2, 3], 'tuple': (5, 6), 'a': {'key': 'value'}}
+file_name = 'data.json'
+
+with open(file_name, "w") as fh:
+    json.dump(some_data, fh)
+
+with open(file_name, "r") as fh:
+    unpacked = json.load(fh)
+
+unpacked is some_data  # False
+unpacked == some_data  # False
+
+unpacked['key'] == some_data['key']  # True
+unpacked['a'] == some_data['a']  # True
+unpacked['2'] == some_data[2]  # True
+unpacked['tuple'] == [5, 6]  # True
+
+The data.json file with the following structure will be the result:
+
+{
+  "key": "value",
+  "2": [
+    1,
+    2,
+    3
+  ],
+  "tuple": [
+    5,
+    6
+  ],
+  "a": {
+    "key": "value"
+  }
+}
+
+                        
+                        Завдання до автоперевірки 2
+                        
+There is a list, each element of which is a dictionary with user contacts of the following type:
+
+{
+    "name": "Allen Raymond",
+    "email": "nulla.ante@vestibul.co.uk",
+    "phone": "(992) 914-3792",
+    "favorite": False,
+}
+The dictionary contains the name of the user name, their email, phone number, and favorite property (whether the contact 
+is a favorite or not).
+
+Develop two functions. One is for serializing and deserializing a list of contacts using a json package, and the other 
+is for storing the resulting data in a text file.
+
+The first function, write_contacts_to_file, takes two parameters: filename — the file’s name, and contacts — the list of 
+contacts. It saves the specified list to a file using the dump method of the json package.
+
+The json file structure should be as follows:
+
+{
+  "contacts": [
+    {
+      "name": "Allen Raymond",
+      "email": "nulla.ante@vestibul.co.uk",
+      "phone": "(992) 914-3792",
+      "favorite": false
+    },
+    ...
+  ]
+}
+That is, the list of contacts should be stored under the "contacts" key, not just save the list to a file.
+
+The second function read_contacts_from_file reads and returns the specified list of contacts from the file filename, previously 
+saved by the write_contacts_to_file function, using the load method of the json package."""
+
+import json
+
+def write_contacts_to_file(filename, contacts):
+    with open(filename, 'w') as file:
+        json.dump({"contacts": contacts}, file)
+    
+
+def read_contacts_from_file(filename):
+    with open(filename, 'r') as file:
+        contacts = json.load(file)["contacts"]
+    return contacts
+
+
+contacts = {
+    "name": "Allen Raymond",
+    "email": "nulla.ante@vestibul.co.uk",
+    "phone": "(992) 914-3792",
+    "favorite": False,
+}
+write_contacts_to_file('contacts.json', contacts)
+print(read_contacts_from_file('contacts.json'))
+# Виведення
+# {'name': 'Allen Raymond', 'email': 'nulla.ante@vestibul.co.uk', 'phone': '(992) 914-3792', 'favorite': False}
