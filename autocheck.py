5aafb5b2bb6fe0366f78162017bbce06b3f722d8
Serg Kalina <kalinasergey911@gmail.com>
2025-09-17 11:52:49 +0300

    Автоперевірка 4

--- autocheck.py
+++ autocheck.py
@@ -360,3 +360,141 @@ print(read_contacts_from_file('contacts.csv'))
 # {'name': 'Wylie Pope', 'email': 'est@utquamvel.net', 'phone': '(692) 802-2949', 'favorite': False}, 
 # {'name': 'Cyrus Jackson', 'email': 'nibh@semsempererat.com', 'phone': '(501) 472-5218', 'favorite': True}]
 
+
+
+"""                                     Автоперевірка 4
+                        
+                        Теорія автоперевірки 4
+                        
+                        
+You can save objects for later use, but only if the class itself has been declared earlier in the code where the unpacking 
+takes place. This is necessary so that pickle can correctly save and then unpack a packed class object.
+
+import pickle
+
+
+class Human:
+    def __init__(self, name):
+        self.name = name
+
+
+bob = Human("Bob")
+encoded_bob = pickle.dumps(bob)
+
+decoded_bob = pickle.loads(encoded_bob)
+
+bob.name == decoded_bob.name  # True
+However, if you wanted to transfer a bob object over the network to another computer that doesn't know anything about the 
+Human class, you would receive an error. If the Human class is declared at both ends of the communication channel, then such 
+an interaction will work.
+
+                        Завдання до автоперевірки 4
+                        
+Develop the Person class. It has four properties: the user's name, their email, phone number phone, and the favorite property 
+(indicating whether the contact is a favorite).
+
+An example of creating a class instance:
+
+    Person(
+    "Allen Raymond",
+    "nulla.ante@vestibul.co.uk",
+    "(992) 914-3792",
+    False,
+)
+Develop the Contacts class. It must initialize two properties through the constructor: filename — the name of the file for packing 
+an object of the Contacts class, contacts — a list of contacts, and instances of the Person class.
+
+An example of creating a class instance:
+
+contacts = [
+    Person(
+        "Allen Raymond",
+        "nulla.ante@vestibul.co.uk",
+        "(992) 914-3792",
+        False,
+    ),
+    Person(
+        "Chaim Lewis",
+        "dui.in@egetlacus.ca",
+        "(294) 840-6685",
+        False,
+    ),
+]
+
+persons = Contacts("user_class.dat", contacts)
+Develop two methods for serializing and deserializing an instance of the Contacts class using the pickle package and 
+storing data in a binary file.
+
+The first method, save_to_file, saves an instance of the Contacts class to a file using the dump method of the pickle 
+package. The file name is stored in the filename attribute.
+
+The second method, read_from_file, reads and returns an instance of the Contacts class from the file filename using the 
+load method of the pickle package.
+
+Example:
+
+persons = Contacts("user_class.dat", contacts)
+persons.save_to_file()
+person_from_file = persons.read_from_file()
+print(persons == person_from_file)  # False
+print(persons.contacts[0] == person_from_file.contacts[0])  # False
+print(persons.contacts[0].name == person_from_file.contacts[0].name)  # True
+print(persons.contacts[0].email == person_from_file.contacts[0].email)  # True
+print(persons.contacts[0].phone == person_from_file.contacts[0].phone)  # True                        """
+
+import pickle
+
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+          
+        
+class Contacts:
+    def __init__(self, filename: str, contacts: list[Person] = None):
+        if contacts is None:
+            contacts = []
+        self.filename = filename
+        self.contacts = contacts
+            
+
+    def save_to_file(self):
+        with open(self.filename, 'wb') as file:
+            pickle.dump(self, file)
+            
+        
+
+    def read_from_file(self):
+        with open(self.filename, 'rb') as file:
+            contacts = pickle.load(file)
+        return contacts
+
+contacts = [
+    Person(
+        "Allen Raymond",
+        "nulla.ante@vestibul.co.uk",
+        "(992) 914-3792",
+        False,
+    ),
+    Person(
+        "Chaim Lewis",
+        "dui.in@egetlacus.ca",
+        "(294) 840-6685",
+        False,
+    ),
+]
+persons = Contacts("user_class.dat", contacts)
+persons.save_to_file()
+person_from_file = persons.read_from_file()
+print(persons == person_from_file)  # False
+print(persons.contacts[0] == person_from_file.contacts[0])  # False
+print(persons.contacts[0].name == person_from_file.contacts[0].name)  # True
+print(persons.contacts[0].email == person_from_file.contacts[0].email)  # True
+print(persons.contacts[0].phone == person_from_file.contacts[0].phone)  # True  
+print(persons.contacts[0].favorite == person_from_file.contacts[0].favorite)  # True
+
+
+    
