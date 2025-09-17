72f75a7dda86bae32745955e8ab6cec6e2168e9c
Serg Kalina <kalinasergey911@gmail.com>
2025-09-17 12:10:49 +0300

    Автоперевірка 5

--- autocheck.py
+++ autocheck.py
@@ -497,4 +497,121 @@ print(persons.contacts[0].phone == person_from_file.contacts[0].phone)  # True
 print(persons.contacts[0].favorite == person_from_file.contacts[0].favorite)  # True
 
 
-    
+"""                                  Автоперевірка 5
+
+                        Теорія автоперевірки 5
+                        
+Not all Python objects can be serialized. For example, you can't serialize a file descriptor or a system resource. 
+What if you have a class whose object you want to package using a pickle, but it has attributes that are not serializable? 
+In this situation, you can use the magic methods that control serialization and deserialization with pickle.
+
+The magic __getstate__ method is called when pickle tries to get a representation of an object as a byte string. In a normal 
+implementation, __getstate__ returns a __dict__ dictionary where all the attributes of the class are stored. But you can 
+change this method.
+
+import pickle
+
+
+class Reader:
+    def __init__(self, file):
+        self.file = file
+        self.fh = open(self.file)
+        self.position = 0
+
+    def close(self):
+        self.fh.close()
+
+    def read(self, size=1):
+        data = self.fh.read(size)
+        self.position = self.fh.tell()
+        return data
+
+    def __getstate__(self):
+        attributes = self.__dict__.copy()
+        attributes['fh'] = None
+        return attributes
+In this example, the Reader class can be serialized, and no errors will occur due to the inability to pack the file descriptor.
+
+
+                        Завдання до автоперевірки 5
+                        
+
+We will continue to expand the example from the previous task. Add the count_save attribute to the Contacts class. It should be set to 0 by default. Implement the magic __getstate__ method for the Contacts class. When packing an instance, the class method should increase the value of the count_save attribute by one. Thus, this property is a counter for repeated operations of packing an instance of a class.
+
+Example:
+
+persons = Contacts("user_class.dat", contacts)
+persons.save_to_file()
+first = persons.read_from_file()
+first.save_to_file()
+second = first.read_from_file()
+second.save_to_file()
+third = second.read_from_file()
+
+print(persons.count_save)  # 0
+print(first.count_save)  # 1
+print(second.count_save)  # 2
+print(third.count_save)  # 3""" 
+
+import pickle
+
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+        
+class Contacts:
+    def __init__(self, filename: str, contacts: list[Person] = None):
+        if contacts is None:
+            contacts = []
+        self.filename = filename
+        self.contacts = contacts
+        self.count_save = 0
+        
+    def save_to_file(self):
+        with open(self.filename, "wb") as file:
+            pickle.dump(self, file)
+
+    def read_from_file(self):
+        with open(self.filename, "rb") as file:
+            content = pickle.load(file)
+        return content
+
+    def __getstate__(self):
+        attributes = self.__dict__.copy()
+        if 'count_save' not in attributes:
+            attributes['count_save'] = 0
+        attributes['count_save'] += 1
+        return attributes
+
+contacts = [
+    Person(
+        "Allen Raymond",
+        "nulla.ante@vestibul.co.uk",
+        "(992) 914-3792",
+        False,
+    ),
+    Person(
+        "Chaim Lewis",
+        "dui.in@egetlacus.ca",
+        "(294) 840-6685",
+        False,
+    ),
+]
+persons = Contacts("user_class.dat", contacts)
+persons.save_to_file()
+first = persons.read_from_file()
+first.save_to_file()
+second = first.read_from_file()
+second.save_to_file()
+third = second.read_from_file()
+print(persons.count_save)  # 0
+print(first.count_save)  # 1
+print(second.count_save)  # 2
+print(third.count_save)  # 3
+
+       
+           
