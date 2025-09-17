9d9626d32bbae31553d9c1518918d33695c1567a
Serg Kalina <kalinasergey911@gmail.com>
2025-09-17 12:57:18 +0300

    Автоперевірка 6

--- autocheck.py
+++ autocheck.py
@@ -614,4 +614,102 @@ print(second.count_save)  # 2
 print(third.count_save)  # 3
 
        
-           
+"""                                  Автоперевірка 6
+
+                        Теорія автоперевірки 6
+                        
+This magic method takes as input a dictionary unpacked from a file or byte string. The default behavior is to write the resulting value to self.__dict__. Let's modify the Reader class so that it can continue reading from the same place after unpacking.
+
+import pickle
+
+
+class Reader:
+    def __init__(self, file):
+        self.file = file
+        self.fh = open(self.file)
+        self.position = 0
+
+    def close(self):
+        self.fh.close()
+
+    def read(self, size=1):
+        data = self.fh.read(size)
+        self.position = self.fh.tell()
+        return data
+
+    def __getstate__(self):
+        attributes = {**self.__dict__}
+        attributes['fh'] = None
+        return attributes
+
+    def __setstate__(self, value):
+        self.__dict__ = value
+        self.fh = open(value['file'])
+        self.fh.seek(value['position'])
+        
+        
+                                Завдання до автоперевірки 6
+                                
+Let's continue to extend the example from the previous task. Add the is_unpacking attribute to the Contacts class, 
+which should be set to False by default. Implement the __setstate__ magic method for the Contacts class. When unpacking 
+an instance of the class, the method should change the value of the is_unpacking attribute to True. Thus, this property 
+will determine that the class instance was obtained as a result of unpacking.
+
+Example:
+
+persons = Contacts("user_class.dat", contacts)
+persons.save_to_file()
+person_from_file = persons.read_from_file()
+print(persons.is_unpacking)  # False
+print(person_from_file.is_unpacking)  # True """
+import pickle
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+class Contacts:
+    def __init__(self, filename: str, contacts: list[Person] = None):
+        if contacts is None:
+            contacts = []
+        self.filename = filename
+        self.contacts = contacts
+        self.count_save = 0
+        self.is_unpacking = False
+    def save_to_file(self):
+        with open(self.filename, "wb") as file:
+            pickle.dump(self, file)
+    def read_from_file(self):
+        with open(self.filename, "rb") as file:
+            content = pickle.load(file)
+        return content
+    def __getstate__(self):
+        attributes = self.__dict__.copy()
+        if 'count_save' not in attributes:
+            attributes['count_save'] = 0
+        attributes['count_save'] += 1
+        return attributes
+    def __setstate__(self, value):
+        self.__dict__ = value
+        self.is_unpacking = True
+
+contacts = [
+    Person(
+        "Allen Raymond",
+        "nulla.ante@vestibul.co.uk",
+        "(992) 914-3792",
+        False,
+    ),
+    Person(
+        "Chaim Lewis",
+        "dui.in@egetlacus.ca",
+        "(294) 840-6685",
+        False,
+    ),
+]
+persons = Contacts("user_class.dat", contacts)
+persons.save_to_file()
+person_from_file = persons.read_from_file()
+print(persons.is_unpacking)  # False
+print(person_from_file.is_unpacking)  # True
